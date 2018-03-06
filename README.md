556dfa24fdde66ab55aaa80ad7c559b3c158c599
Ashley Trinh <ashqtrinh@gmail.com>
2018-03-06 13:37:10 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+![screenshot](screenshots/hyperterm.png)
+
+# hyper-mahoushoujo
+A bright, energetic, light theme for [Hyper.app](https://hyper.is), inspired
+by magical girls ✨
+
+![colors](screenshots/themecolors.png)
+
+![vim](screenshots/vim.png)
+
+### installation
+In your `.hyper.js`, add the package name to your plugins array:
+
+```js
+plugins: [
+  'hyper-teatime',
+]
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+exports.decorateConfig = config => { Object.assign({}, config, {
+    foregroundColor: '#453d40',
+    backgroundColor: '#ffffff',
+    cursorColor: '#abc8ff',
+    colors: {
+      black: '#abc8ff',
+      lightBlack: '#a1a2fb',
+      red: '#ff98c5',
+      lightRed: '#ff71b5',
+      green: '#54d7d0',
+      lightGreen: '#27b2ca',
+      yellow: '#fcd198',
+      lightYellow: '#fca760',
+      blue: '#93e0ff',
+      lightBlue: '#6693b3',
+      magenta: '#9999db',
+      lightMagenta: '#7d69d2',
+      cyan: '#c9acca',
+      lightCyan: '#d56fa9',
+      white: '#807dc3',
+      lightWhite: '#3a3690'
+    }
+ })
+};
