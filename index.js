4da055ae57574d940c40e95da8c2fbf50612977e
Ashley Trinh <ashqtrinh@gmail.com>
2018-03-07 13:12:52 -0800

    Fix decorateConfig

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-exports.decorateConfig = config => { Object.assign({}, config, {
-    foregroundColor: '#453d40',
-    backgroundColor: '#ffffff',
-    cursorColor: '#abc8ff',
-    colors: {
-      black: '#abc8ff',
-      lightBlack: '#a1a2fb',
-      red: '#ff98c5',
-      lightRed: '#ff71b5',
-      green: '#54d7d0',
-      lightGreen: '#27b2ca',
-      yellow: '#fcd198',
-      lightYellow: '#fca760',
-      blue: '#93e0ff',
-      lightBlue: '#6693b3',
-      magenta: '#9999db',
-      lightMagenta: '#7d69d2',
-      cyan: '#c9acca',
-      lightCyan: '#d56fa9',
-      white: '#807dc3',
-      lightWhite: '#3a3690'
-    }
+const cursorColor = '#abc8ff';
+const foregroundColor = '#453d40';
+const backgroundColor = '#ffffff';
+const colors = {
+  black: '#abc8ff',
+  lightBlack: '#a1a2fb',
+  red: '#ff98c5',
+  lightRed: '#ff71b5',
+  green: '#54d7d0',
+  lightGreen: '#27b2ca',
+  yellow: '#fcd198',
+  lightYellow: '#fca760',
+  blue: '#93e0ff',
+  lightBlue: '#6693b3',
+  magenta: '#9999db',
+  lightMagenta: '#7d69d2',
+  cyan: '#c9acca',
+  lightCyan: '#d56fa9',
+  white: '#807dc3',
+  lightWhite: '#3a3690'
+};
+
+exports.decorateConfig = config => { 
+  return Object.assign({}, config, {
+    foregroundColor,
+    backgroundColor,
+    cursorColor,
+    colors
  })
 };
+
